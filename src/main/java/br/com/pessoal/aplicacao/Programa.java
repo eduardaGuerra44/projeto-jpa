b8dc6ab002f2e2c0326fd7d740c4ae50f6588bfe
Eduarda Guerra <eduardaguerra44@gmail.com>
2024-02-15 14:25:26 -0300

    mapeando objetos com jpa

--- src/main/java/br/com/pessoal/aplicacao/Programa.java
+++ src/main/java/br/com/pessoal/aplicacao/Programa.java
@@ -1,15 +1,28 @@
 package br.com.pessoal.aplicacao;
 
 import br.com.pessoal.modelo.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Programa {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Maria Guerra", "mariaguerra@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Matheus Henrique", "matheusgiancott@gmai.com");
-        Pessoa p3 = new Pessoa(3, "Lia Guerra", "liaguerra@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+
+        //instanciando o entity-manager-factory
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        //instanciando o entity-manager-factory
+        EntityManager em = emf.createEntityManager();
+
+
+        Pessoa p = em.find(Pessoa.class, 2);
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+        System.out.println("pronto");
+        em.close();
+        emf.close();
+
+
     }
 }
--- src/main/java/br/com/pessoal/modelo/Pessoa.java
+++ src/main/java/br/com/pessoal/modelo/Pessoa.java
@@ -1,11 +1,20 @@
 package br.com.pessoal.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable{
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
+
 
     public Pessoa(){
 
